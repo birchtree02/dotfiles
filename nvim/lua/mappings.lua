784e48923b0774c6c7e952df435ffad323337e96
Edward Birchall <edward@birchall.org.uk>
2024-11-08 16:48:36 +0000

    fix tmux mappings

--- nvim/lua/mappings.lua
+++ nvim/lua/mappings.lua
@@ -8,3 +8,10 @@ map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
+
+local opts = { noremap = true, silent = true }
+
+map('n', '<C-h>', ':TmuxNavigateLeft<CR>', opts)
+map('n', '<C-j>', ':TmuxNavigateDown<CR>', opts)
+map('n', '<C-k>', ':TmuxNavigateUp<CR>', opts)
+map('n', '<C-l>', ':TmuxNavigateRight<CR>', opts)

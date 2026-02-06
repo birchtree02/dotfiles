a1e43616e90e59b92c70dee256934786d22cd579
Edward Birchall <nbircedw@amazon.com>
2026-02-06 16:02:11 +0000

    disable undo if filepath too long

--- nvim/lua/options.lua
+++ nvim/lua/options.lua
@@ -5,3 +5,13 @@ require "nvchad.options"
 -- local o = vim.o
 -- o.cursorlineopt ='both' -- to enable cursorline!
 vim.opt.relativenumber = true
+
+-- Disable undo file if path is too long
+vim.api.nvim_create_autocmd("BufWritePre", {
+  callback = function()
+    local undofile = vim.fn.undofile(vim.fn.expand("%"))
+    if #undofile > 255 then
+      vim.bo.undofile = false
+    end
+  end,
+})

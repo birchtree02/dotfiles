b91ac6051d7bf84e8aa0b068c48562b69c64f61d
Edward Birchall <nbircedw@amazon.com>
2026-02-16 17:08:34 +0000

    Add (empty) cloudsoft config

--- nvim/init.lua
+++ nvim/init.lua
@@ -3,6 +3,7 @@ require("config.theme")
 require("config.options")
 require("config.keymaps")
 require("config.lsp")
+require("config.cloudsoft")
 
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1

256707ef5d359e71140aa68e29987801470c1281
Edward Birchall <nbircedw@amazon.com>
2026-03-03 16:06:37 +0000

    add Comment.nvim

---
+++ nvim/lua/plugins/comment.lua
@@ -0,0 +1,10 @@
+return {
+	"numToStr/Comment.nvim",
+	keys = {
+		{ "gcc", mode = "n", desc = "Toggle line comment" },
+		{ "gbc", mode = "n", desc = "Toggle block comment" },
+		{ "gc", mode = "v", desc = "Toggle line comment" },
+		{ "gb", mode = "v", desc = "Toggle block comment" },
+	},
+	opts = {},
+}

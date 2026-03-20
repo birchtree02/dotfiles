35f7fb4fd1431c03bc4eb11377faf2bdfe314803
Edward Birchall <nbircedw@amazon.com>
2026-03-20 14:58:04 +0000

    theme switching (<leader>+T)

--- nvim/lua/config/theme.lua
+++ nvim/lua/config/theme.lua
@@ -1,2 +1,17 @@
-vim.o.termguicolors = true -- https://gist.github.com/andersevenrud/015e61af2fd264371032763d4ed965b6
-vim.cmd.colorscheme("catppuccin")
+vim.o.termguicolors = true
+
+local function get_flavor()
+  local link = vim.fn.resolve(vim.fn.expand("~/.config/kitty/catppuccin.conf"))
+  return link:match("latte") and "latte" or "mocha"
+end
+
+local function apply_theme()
+  require("catppuccin").setup({ flavour = get_flavor() })
+  vim.cmd.colorscheme("catppuccin")
+end
+
+apply_theme()
+
+vim.api.nvim_create_autocmd("FocusGained", {
+  callback = apply_theme,
+})
---
+++ scripts/toggle-theme.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+KITTY_DIR="$HOME/.config/kitty"
+TMUX_DIR="$HOME/.config/tmux"
+
+current=$(readlink "$KITTY_DIR/catppuccin.conf" 2>/dev/null)
+if [[ "$current" == *latte* ]]; then
+  flavor="mocha"
+else
+  flavor="latte"
+fi
+
+ln -sf "catppuccin-$flavor.conf" "$KITTY_DIR/catppuccin.conf"
+ln -sf "catppuccin-$flavor.conf" "$TMUX_DIR/catppuccin.conf"
+
+kill -SIGUSR1 $(pgrep -x kitty) 2>/dev/null
+tmux source-file "$TMUX_DIR/tmux.conf" 2>/dev/null
+
+echo "Switched to catppuccin-$flavor"

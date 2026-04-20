ef472a37df2814f2f70b5d250360a1319ca5395d
Edward Birchall <nbircedw@amazon.com>
2026-04-20 12:05:25 +0100

    kitty super-n tab switch

--- nvim/lua/plugins/snacks.lua
+++ nvim/lua/plugins/snacks.lua
@@ -1,3 +1,27 @@
+local todo_cache = {}
+
+local function update_todos()
+	local cmd =
+		[[grep -rn --exclude-dir build 'TO''DO-' . | awk -F : '{printf("%-60s %s:%s\n", substr($0, index($0, "TO" "DO")), $1, $2);}' | sort -t - -k 2 -k 4 -k 6 -n]]
+	vim.fn.jobstart(cmd, {
+		stdout_buffered = true,
+		on_stdout = function(_, data)
+			local items = {}
+			for _, line in ipairs(data) do
+				if line ~= "" then
+					local file, lnum = line:match("%s+([^:]+):(%d+)$")
+					local text = line:match("^(.-)%s+[^%s]+$")
+					if file and lnum and text then
+						file = file:gsub("^%./", "")
+						table.insert(items, { file = file, text = vim.trim(text), pos = { tonumber(lnum), 0 } })
+					end
+				end
+			end
+			todo_cache = items
+		end,
+	})
+end
+
 return {
 	"folke/snacks.nvim",
 	priority = 1000,
@@ -6,6 +30,12 @@ return {
 		picker = {},
 		explorer = {},
 	},
+	init = function()
+		update_todos()
+		vim.api.nvim_create_autocmd("BufWritePost", {
+			callback = update_todos,
+		})
+	end,
 	keys = {
 		-- Top Pickers & Explorer
 		{
@@ -50,11 +80,27 @@ return {
 			desc = "Notification History",
 		},
 		{
-			"<leader>e",
+			"<leader>ft",
 			function()
-				Snacks.explorer()
+				Snacks.picker.pick({
+					items = todo_cache,
+					format = function(item)
+						local filename = item.file:match("([^/]+)$")
+						return { { item.text .. " " .. filename .. ":" .. item.pos[1] } }
+					end,
+					win = {
+						input = {
+							keys = {
+								i_esc = { "<esc>", "close", mode = "i" },
+							},
+						},
+					},
+				})
+				vim.schedule(function()
+					vim.cmd("stopinsert")
+				end)
 			end,
-			desc = "File Explorer",
+			desc = "Find TODOs (sorted)",
 		},
 	},
 }

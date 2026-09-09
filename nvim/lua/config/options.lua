eb7df9c980a19737edd4ecd8de8685f28b232e0d
Edward Birchall <nbircedw@amazon.com>
2026-09-09 14:17:03 +0000

    [nvim] don't duplicate wrapped line numbers

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -21,7 +21,7 @@ opt.autowrite = true -- Auto write on change buffer
 opt.completeopt = "menu,menuone,noselect"
 opt.number = true
 opt.relativenumber = true
-opt.statuscolumn = "%s%{v:lnum} %=%{v:relnum?v:relnum:v:lnum} "
+opt.statuscolumn = "%s%{v:virtnum>0?'│':v:lnum} %=%{v:virtnum>0?'':(v:relnum?v:relnum:v:lnum)} "
 opt.tabstop = 2 -- number of spaces tabs count for
 opt.shiftwidth = 2 -- size of an indent
 opt.expandtab = true -- Use spaces instead of tabs
